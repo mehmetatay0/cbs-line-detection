a7f604787dfb6e9ef01a0ac007bb6d2d8098a3e8
mehmetatay0 <mehmetatay257@gmail.com>
2020-11-19 22:06:18 +0300

    Found that points of point on the circle

--- main.py
+++ main.py
@@ -12,18 +12,28 @@ def main():
 
     # Step of Circle Based Search
     imgCBS = filtered.copy()
-    copyImg = drawImage(imgCBS, 150, 150, 100, 255)
-    
-    showImg(copyImg)
+    copyImg = drawImage(imgCBS, 255)
+
+    # showImg(copyImg)
 
 
 # Bresenham's Cicle
-def drawImage(img, xc, yc, r, color):
+def drawImage(img, color):
+    xc, yc = np.shape(img)
+    xc = int(xc / 2)
+    yc = int(yc / 2)
+    r = int(0.8 * np.amin([xc, yc]))
+
     d = 3 - (2 * r)
     x = 0
     y = r
+
+    # Points could be our line
+    x_points = np.array([])
+    y_points = np.array([])
+
     while(True):
-        img = drawCircle(img, xc, yc, x, y, color)
+        x_points, y_points = controlOnCircle(img, xc, yc, x, y, x_points, y_points)
         if (x <= y):
             x += 1
             if (d < 0):
@@ -33,19 +43,58 @@ def drawImage(img, xc, yc, r, color):
                 y -= 1
         else:
             break
+    print(x_points)  # That's x points of the lines or any points
+    print(y_points)  # That's y points of the lines or any points
     return img
 
 
-def drawCircle(img,xc, yc, x, y, color):
-    img[xc + x][yc + y] = color  # 1 of octans
-    img[xc - x][yc + y] = color  # 2 of octans
-    img[xc + x][yc - y] = color  # 3 of octans
-    img[xc - x][yc - y] = color  # 4 of octans
-    img[xc + y][yc + x] = color  # 5 of octans
-    img[xc - y][yc + x] = color  # 6 of octans
-    img[xc + y][yc - x] = color  # 7 of octans
-    img[xc - y][yc - x] = color  # 8 of octans
-    return img
+def controlOnCircle(img, xc, yc, x, y, x_points, y_points):
+    if (img[xc + x][yc + y] == 255):            # 1 of octans
+        x_points = np.append(x_points, xc + x)
+        y_points = np.append(y_points, yc + y)
+
+    elif (img[xc - x][yc + y] == 255):          # 2 of octans
+        x_points = np.append(x_points, xc - x)
+        y_points = np.append(y_points, yc + y)
+
+    elif (img[xc + x][yc - y] == 255):          # 3 of octans
+        x_points = np.append(x_points, xc + x)
+        y_points = np.append(y_points, yc - y)
+
+    elif (img[xc - x][yc - y] == 255):          # 4 of octans
+        x_points = np.append(x_points, xc - x)
+        y_points = np.append(y_points, yc - y)
+
+    elif (img[xc + y][yc + x] == 255):          # 5 of octans
+        x_points = np.append(x_points, xc + y)
+        y_points = np.append(y_points, yc + x)
+
+    elif (img[xc - y][yc + x] == 255):          # 6 of octans
+        x_points = np.append(x_points, xc - y)
+        y_points = np.append(y_points, yc + x)   
+
+    elif (img[xc + y][yc - x] == 255):          # 7 of octans
+        x_points = np.append(x_points, xc + y)
+        y_points = np.append(y_points, yc - x)
+
+    elif (img[xc - y][yc - x] == 255):          # 8 of octans
+        x_points = np.append(x_points, xc - y)
+        y_points = np.append(y_points, yc - x)
+
+    return [x_points, y_points]
+
+# def drawCircle(img,xc, yc, x, y, color):
+#     img[xc + x][yc + y] = color  # 1 of octans
+#     img[xc - x][yc + y] = color  # 2 of octans
+#     img[xc + x][yc - y] = color  # 3 of octans
+#     img[xc - x][yc - y] = color  # 4 of octans
+#     img[xc + y][yc + x] = color  # 5 of octans
+#     img[xc - y][yc + x] = color  # 6 of octans
+#     img[xc + y][yc - x] = color  # 7 of octans
+#     img[xc - y][yc - x] = color  # 8 of octans
+#     return img
+
+
 
 
 def showImg(img):
